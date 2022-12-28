48669b5f8548a224420c8f280103081a3fded96c
Harsh kumar <hk7206210@gmail.com>
2022-12-28 13:18:32 +0530

    add navbar

--- react_chat/src/components/Navbar.js
+++ react_chat/src/components/Navbar.js
@@ -12,7 +12,7 @@ export function Navbar() {
         <div className="max-w-5xl mx-auto flex flex-wrap justify-between items-center">
           <Link to="/" className="flex items-center">
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Conversa DJ
+              Django React Chat
             </span>
           </Link>
           <button
